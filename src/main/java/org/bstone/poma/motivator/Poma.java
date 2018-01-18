789c0af317ea1f15734debf785db18393ac36780
Andrew <andykuo1supergreen@gmail.com>
2018-01-18 10:09:04 -0800

    Ignore lib files

---
+++ src/main/java/org/bstone/poma/motivator/Poma.java
@@ -0,0 +1,12 @@
+package org.bstone.poma.motivator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Andy on 1/18/18.
+ */
+public class Poma
+{
+	public static final Logger LOG = LoggerFactory.getLogger(Poma.class);
+}
